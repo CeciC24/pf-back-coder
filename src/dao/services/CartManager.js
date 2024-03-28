cf999af68919a91345eb4c3f6e68ea931ce8ba1d
CeciC24 <ccovalchuk@frba.utn.edu.ar>
2024-03-28 13:17:12 -0300

    Métodos update y optimizaciones

--- src/dao/services/CartManager.js
+++ src/dao/services/CartManager.js
@@ -27,16 +27,14 @@ class CartManager {
 
 		if (cart) {
 			if (product) {
-				let productInCart = cart.products.find((p) => p.product.toString() == pid.toString())
+				let productInCart = cart.products.find((p) => p.product._id == pid)
 
 				if (productInCart) {
 					productInCart.quantity += 1
-                    cart.markModified('products')
 				} else {
 					cart.products.push({ product: pid, quantity: 1 })
 				}
 
-				await cart.save()
 			} else {
 				throw new Error(`⚠️  Product ID: ${pid} Not found`)
 			}
@@ -44,24 +42,22 @@ class CartManager {
 			throw new Error(`⚠️  Cart ID: ${cid} Not found`)
 		}
 
-		let updatedCart = await this.getCartById(cid)
-		return updatedCart
+		return await cart.save()
 	}
 
 	async deleteProductFromCart(cid, pid) {
 		let cart = await this.getCartById(cid)
 
 		if (cart) {
-			let productInCart = cart.products.find((p) => p.product.toString() == pid.toString())
+			let productInCart = cart.products.find((p) => p.product._id == pid)
 
 			if (productInCart) {
 				if (productInCart.quantity > 1) {
 					productInCart.quantity -= 1
-					cart.markModified('products')
 				} else {
 					cart.products.pull(productInCart)
 				}
-				await cart.save()
+				
 			} else {
 				throw new Error(`⚠️  Product ID: ${pid} Not found`)
 			}
@@ -69,13 +65,53 @@ class CartManager {
 			throw new Error(`⚠️  Cart ID: ${cid} Not found`)
 		}
 
-		let updatedCart = await this.getCartById(cid)
-		return updatedCart
+		return await cart.save()
+	}
+
+	async updateCart(cid, newCartProducts) {
+		const cart = await this.getCartById(cid)
+
+		newCartProducts.forEach((newProduct) => {
+			let productInCart = cart.products.find((p) => p.product._id == newProduct.product._id)
+
+			if (productInCart) {
+				productInCart.quantity = newProduct.quantity
+			} else {
+				this.addProductToCart(cid, newProduct.product._id)
+			}
+		})
+
+		return await cart.save()
+	}
+
+	async updateProductInCart(cid, pid, newQuantity) {
+		let cart = await this.getCartById(cid)
+		let product = await ProductMngr.getProductById(pid)
+
+		if (cart) {
+			if (product) {
+				let productInCart = cart.products.find((p) => p.product._id == pid)
+
+				if (productInCart) {
+					productInCart.quantity = newQuantity.quantity
+				} else {
+					throw new Error(`⚠️  Product ID: ${pid} Not found in Cart ID: ${cid}`)
+				}
+
+			} else {
+				throw new Error(`⚠️  Product ID: ${pid} Not found`)
+			}
+		} else {
+			throw new Error(`⚠️  Cart ID: ${cid} Not found`)
+		}
+
+		return await cart.save()
 	}
 
 	async emptyCart(id) {
 		let response = await CartsModel.findByIdAndUpdate(id, { products: [] })
-		return response
+		let emptyCart = await this.getCartById(id)
+		return emptyCart
 	}
 }
 
